eb932d97c37a76173cefef7a5da3ff7205506fa9
kehl-gopher <kelanidarasimi9@gmail.com>
2025-07-18 21:19:42 +0100

    chore(log): write helper function to initialize logger

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kehl-gopher/logi
+
+go 1.24.4
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1
---
+++ internal/utils/log.go
@@ -0,0 +1,140 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"log/slog"
+	"os"
+	"sync"
+
+	lg "gopkg.in/natefinch/lumberjack.v2"
+)
+
+type LogLevel int
+
+const (
+	InfoLevel LogLevel = iota + 1
+	DebugLevel
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+)
+
+type Logger interface {
+	infoLog(message string, args ...interface{})
+	warnLog(message string, args ...interface{})
+	debugLog(message string, args ...interface{})
+	errorLog(message string, args ...interface{})
+}
+
+type fLogger interface {
+	fatalLog(message string, args ...interface{})
+}
+
+type Log struct {
+	lmutex *sync.Mutex
+	log    *slog.Logger
+	fLog   fLogger
+}
+
+type fLog struct {
+	log *slog.Logger
+}
+
+func newFlogger(l *lg.Logger) fLogger {
+	faLog := slog.New(slog.NewJSONHandler(l, &slog.HandlerOptions{
+		Level: slog.LevelError,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.LevelKey && a.Value.String() == slog.LevelError.String() {
+				return slog.String(a.Key, "FATAL")
+			}
+			return a
+		},
+	}))
+	return &fLog{log: faLog}
+}
+func NewLogger(mode string) Logger {
+	lumLog := &lg.Logger{
+		Filename:   "./logs/app.log",
+		MaxSize:    10,
+		MaxBackups: 3,
+		MaxAge:     30,
+	}
+	handler := slog.New(slog.NewJSONHandler(lumLog, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	faLog := newFlogger(lumLog)
+	return &Log{log: handler, fLog: faLog, lmutex: new(sync.Mutex)}
+}
+
+func (l *Log) infoLog(message string, args ...interface{}) {
+	l.lmutex.Lock()
+	defer l.lmutex.Unlock()
+	if args == nil {
+		l.log.Info(message)
+		return
+	}
+	l.log.Info(message, args...)
+}
+
+func (l *Log) debugLog(message string, args ...interface{}) {
+	l.lmutex.Lock()
+	defer l.lmutex.Unlock()
+	if args == nil {
+		l.log.Debug(message)
+		return
+	}
+	l.log.Info(message)
+}
+
+func (l *Log) warnLog(message string, args ...interface{}) {
+	l.lmutex.Lock()
+	defer l.lmutex.Unlock()
+	if args == nil {
+		l.log.Warn(message)
+		return
+	}
+	l.log.Warn(message, args...)
+}
+
+func (l *Log) errorLog(message string, args ...interface{}) {
+	l.lmutex.Lock()
+	defer l.lmutex.Unlock()
+	if args == nil {
+		l.log.Error(message)
+		return
+	}
+	l.log.Error(message, args...)
+}
+
+func (l *fLog) fatalLog(message string, args ...interface{}) {
+	if args == nil {
+		l.log.Error(message)
+	}
+	l.log.Error(message, args...)
+}
+
+func PrintLog(log *Log, message string, level LogLevel, args ...interface{}) {
+	switch level {
+	case 1:
+		log.infoLog(message, args...)
+	case 2:
+		log.debugLog(message, args...)
+	case 3:
+		log.warnLog(message, args...)
+	case 4:
+		log.errorLog(message, args...)
+	case 5:
+		log.lmutex.Lock()
+		log.fLog.fatalLog(message, args)
+		log.lmutex.Unlock()
+	}
+	
+	log.lmutex.Lock()
+	buf := bufio.NewWriter(os.Stdout)
+	buf.Write([]byte(message))
+	fmt.Fprintln(buf, args...)
+	buf.Flush()
+	log.lmutex.Unlock()
+}
